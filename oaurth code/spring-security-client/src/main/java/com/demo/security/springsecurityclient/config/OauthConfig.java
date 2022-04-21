c44d71945d520d9465ea561db5867d8499c6dec2
unknown <mantha747@gmail.com>
2022-04-21 04:59:23 -0700

    oaurth demo

---
+++ oaurth code/spring-security-client/src/main/java/com/demo/security/springsecurityclient/config/OauthConfig.java
@@ -0,0 +1,24 @@
+package com.demo.security.springsecurityclient.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableOAuth2Sso
+@Configuration
+public class OauthConfig extends WebSecurityConfigurerAdapter{
+
+// implemented spring security here
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+    }
+}
